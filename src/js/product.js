734e5b319ac7311fd8f1be1de690839566299b13
Nate J <46363319+Millwr1ght@users.noreply.github.com>
2023-01-16 20:20:53 -0600

    run  npm line and format

--- src/js/product.js
+++ src/js/product.js
@@ -3,6 +3,6 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productID = getParams('product');
+const productID = getParams("product");
 const product = new ProductDetails(dataSource, productID);
 product.init();
