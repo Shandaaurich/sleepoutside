d20cbeab3f51a293da0f82a9668c7f50da73c4de
ShandaAurich <shanda@aurich.co>
2023-01-20 13:40:30 -0800

    test alert working on index page

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,34 +1,28 @@
+import alertData from "../json/alerts.json";
 
-export default class Alerts {
+export default class Alert {
   constructor() {
-    //read alerts.js file
-    // let alerts = require("../json/alerts.json");
-    // console.log(alerts);
-    getData() {
-        return fetch("../json/alerts.json")
-          .then(convertToJson)
-          .then((data) => data);
-          return data;
-      }
-
-    //create a <section class="alert-list>
-    let alertSection = document.createElement("section");
-    alertSection.className = "alert-list";
-
-    //loop through the results and create a <p> for each alert
-    for (let alert of alerts) {
-      let alertP = document.createElement("p");
-      alertP.innerHTML = alert.message;
-
-      //apply background & foreground colors
-      alertP.style.backgroundColor = alert.backgroundColor;
-      alertP.style.color = alert.foregroundColor;
-
-      //append <p> to <section>
-      alertSection.appendChild(alertP);
-    }
-
-    //prepend <section> to main element
-    document.getElementById("main").prepend(alertSection);
+    this.dataSource = alertData;
   }
+
+  renderAlertDetails() {
+    //select the main element of the page and prepend the alert sections to it
+    const element = document.querySelector("main");
+    element.prepend(alertTemplate(this.dataSource));
+  }
+}
+
+function alertTemplate(dataSource) {
+  //if the dataSource has contents then create a section with class "alert-list"
+  const alertSection = document.createElement("section");
+  alertSection.classList.add("alertSection");
+  //loop throught tresults and build a p for each alert. Apply the background and foreground colors specified in the alerts.json file
+  dataSource.forEach((alertMessage) => {
+    const alertP = document.createElement("p");
+    alertP.innerText = alertMessage.message;
+    alertP.style.backgroundColor = alertMessage.background;
+    alertP.style.color = alertMessage.color;
+    alertSection.appendChild(alertP);
+  });
+  return alertSection;
 }
---
+++ src/js/displayAlerts.js
@@ -0,0 +1,4 @@
+import Alert from "../js/Alert";
+
+const alert = new Alert();
+alert.renderAlertDetails();
