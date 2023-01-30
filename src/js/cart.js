1c5ab43101808e70688d0e547e43d58a1ac35c4c
ShandaAurich <shanda@aurich.co>
2023-01-30 11:23:55 -0800

    remove item and re-render cart list. needs refresh

--- src/js/cart.js
+++ src/js/cart.js
@@ -13,11 +13,16 @@ function renderCartContents() {
     const htmlItems = cartEmptyTemplate();
     qs(".product-list").innerHTML = htmlItems;
   } else if (cartItems != null) {
+    
     // else if not empty display cart contents
     renderListWithTemplate(cartItemTemplate, qs(".product-list"), cartItems);
+
+    removeItems();
     // display total
     qs(".cart-total").innerHTML = `Cart Total: $<strong>${cartTotal}</strong>`;
+
   }
+
 }
 
 function cartEmptyTemplate() {
@@ -55,8 +60,45 @@ function cartItemTemplate(item) {
   
   <p class="cart-card__price">$${item.FinalPrice}</p>
   </li>`;
+
   cartTotal += item.FinalPrice;
   return newItem;
+}
+
+function removeItems() {
+  const selectRemoveLinks = document.querySelectorAll(".cart-card__delete");
+  
+  for (let link of selectRemoveLinks) {
+
+    //attach a listener to each remove link
+    link.addEventListener("click", function(e) {
+      
+      //pull the Id of the item to be removed
+      const currentArticle = e.currentTarget;
+      let itemId = currentArticle.dataset.id
+      console.log(itemId);
+      
+      //pull the contents of the cart from local storage  
+      let cartContents = JSON.parse(localStorage.getItem("so-cart"));
+
+      //remove the appropriate item      
+      let itemIndex = cartContents.findIndex(item => item.id === itemId);
+      cartContents.splice(itemIndex, 1);
+
+      //Clear cart contents
+      qs(".product-list").innerHTML = "";
+      //set local storage with list after splice
+      localStorage.setItem("so-cart", JSON.stringify(cartContents));
+      //pull down the contents of the new local storage
+      let newCart = getLocalStorage("so-cart");
+      //re-render the cart list
+      renderListWithTemplate(cartItemTemplate, qs(".product-list"), newCart);
+      //update cart icon
+      initCartIcon();
+    });
+    
+  };
+    
 }
 
 renderCartContents();
