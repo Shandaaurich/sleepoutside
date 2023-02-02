6c3bbb57885f652a2c3ad55d765ab109c617921f
TristinParker <gidgidonihah@byui.edu>
2023-02-01 20:18:10 -0700

    fixes

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,14 @@
-import { getLocalStorage, renderListWithTemplate, qs, loadHeaderFooter } from "./utils.mjs";
+import {
+  getLocalStorage,
+  renderListWithTemplate,
+  qs,
+  loadHeaderFooter,
+} from "./utils.mjs";
 import { initCartIcon } from "./Cart.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 // show number of items in cart on the cart icon in header
 loadHeaderFooter(null, initCartIcon);
 
-const cart = new ShoppingCart("so-cart", qs(".product-list"))
-cart.init()
+const cart = new ShoppingCart("so-cart", qs(".product-list"));
+cart.init();
