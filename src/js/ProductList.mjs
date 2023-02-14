740f4ecaf90fc227c2cffee3f53ebf52855f248a
TristinParker <gidgidonihah@byui.edu>
2023-02-13 18:14:51 -0700

    Update ProductList.mjs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -13,7 +13,7 @@ function productCardTemplate(product) {
     return `<li class="product-card"><a href="/product_pages/index.html?product=${product.Id}"><img
     src="${product.Images.PrimaryMedium}"
     alt="Image of ${product.Name}"
-    /><h3 class="card__brand">${product.Brand.Name}</h3><h2 class="card__name">${product.Name}</h2><p class="product-card__price">$${product.FinalPrice}</p>${discountHTML}</a></li>
+    /><h3 class="card__brand">${product.Brand.Name}</h3><h2 class="card__name">${product.NameWithoutBrand}</h2><p class="product-card__price">$${product.FinalPrice}</p>${discountHTML}</a></li>
     
     `;
     
