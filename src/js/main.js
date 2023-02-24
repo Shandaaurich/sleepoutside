6dfa04d153b838fc20cade70d3f5255896cf9ee1
ShandaAurich <shanda@aurich.co>
2023-02-23 21:08:02 -0800

    add register alert and modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import { initCartIcon } from "./Cart.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, alertRegister } from "./utils.mjs";
 
 //call header and footer from the template
 loadHeaderFooter(null, initCartIcon);
+
+//call newsletter alert message from the template
+alertRegister();
+
