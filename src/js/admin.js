281fd215483d8f7bebd52e8d4de7ea100171ab32
TristinParker <gidgidonihah@byui.edu>
2023-02-27 20:03:09 -0700

    Week08 Team Assignment

---
+++ src/js/admin.js
@@ -0,0 +1,12 @@
+import Admin from "./admin.mjs";
+import { initCartIcon } from "./Cart.mjs";
+import {  loadHeaderFooter } from "./utils.mjs";
+
+//const outputSelector = qs("#loginForm");
+
+loadHeaderFooter(null, initCartIcon);
+
+const myAdmin = new Admin("main");
+
+myAdmin.showlogin();
+
