84830941fe116a1fda8c6ba457ec027831525a96
ShandaAurich <shanda@aurich.co>
2023-04-04 18:46:53 -0700

    fixing some filepaths for pages

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -10,7 +10,7 @@ function productCardTemplate(product) {
     } else {
         discountHTML = "";
     }
-    return `<li class="product-card"><a href="/product_pages/index.html?product=${product.Id}"><img
+    return `<li class="product-card"><a href="../product_pages/index.html?product=${product.Id}"><img
     src="${product.Images.PrimaryMedium}"
     alt="Image of ${product.Name}"
     /><h3 class="card__brand">${product.Brand.Name}</h3><h2 class="card__name">${product.NameWithoutBrand}</h2><p class="product-card__price">$${product.FinalPrice}</p>${discountHTML}</a></li>
--- src/js/admin.js
+++ src/js/admin.js
@@ -1,11 +1,11 @@
 import Admin from "./admin.mjs";
 import { initCartIcon } from "./Cart.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+import { qs, loadHeaderFooter } from "./utils.mjs";
 
-//const outputSelector = qs("#loginForm");
+const outputSelector = qs("#loginForm");
 
 loadHeaderFooter(null, initCartIcon);
 
-const myAdmin = new Admin("main");
+const myAdmin = new Admin(outputSelector);
 
 myAdmin.showlogin();
